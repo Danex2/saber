593e33e6166868158e8d080edb3ba807b03b70b8
Danex2 <khadane.miller@gmail.com>
2021-01-24 00:28:43 -0500

    add user roles and avatar link to user embed as well as embed colour

--- commands/user.go
+++ commands/user.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -9,6 +11,12 @@ import (
 )
 
 func init() {
+
+	// Bot.GetCommand("user").(*gommand.Command).Category = &gommand.Category{
+	// 	Name: "Info",
+	// 	Description: "Info commands provide information about the user or the server",
+	// }
+
 	Bot.SetCommand(&gommand.Command{
 		Name: "user",
 		Description: "Get info about the user",
@@ -21,6 +29,27 @@ func init() {
 			userImg, _ := ctx.Message.Author.AvatarURL(2048, false)
 
 
+			// Move this logic into a utility function
+			unixTimeStamp := ((ctx.Message.Author.ID / 4194304) + 1420070400000) / 1000
+
+			parsedTimeStamp, err := strconv.ParseInt(fmt.Sprint(unixTimeStamp), 10, 64)
+
+			if err != nil {
+				panic(err)
+			}
+
+			joinDate := time.Unix(parsedTimeStamp, 0)
+
+			y, _ := time.Parse("2006-01-02 15:04:05 -0700 MST", joinDate.String())
+		
+			// again more clean up probably
+
+			var userRoles []string
+
+			for i := range ctx.Message.Member.Roles {
+				userRoles = append(userRoles, fmt.Sprintf("<@&%s>", ctx.Message.Member.Roles[i]))
+			}
+
 			_, _ = ctx.Reply(&disgord.Embed{
 				Title: "User Details",
 				Fields: []*disgord.EmbedField{{
@@ -29,13 +58,29 @@ func init() {
 				},{
 					Name: "Username",
 					Value: ctx.Message.Member.User.Tag(),
+					Inline: true,
+				}, {
+					Name: "Avatar URL",
+					Value: fmt.Sprintf("[Link](%s)", userImg),
+				}, {
+					Name: "Roles",
+					Value: strings.Join(userRoles, ""),
 				}, {
 					Name: "Server Join Date",
 					Value: t.Format("January 2 2006"),
+					Inline: true,
+				}, {
+					Name: "Account Creation Date",
+					Value: y.Format("January 2 2006"),
+					Inline: true,
 				}},
 				Thumbnail: &disgord.EmbedThumbnail{
 					URL: userImg,
 				},
+				Timestamp: disgord.Time{
+					Time: time.Now().UTC(),
+				},
+				Color: 15724753,
 			})
 
 			return nil
